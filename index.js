47f84503e88b4a80ba29f8b9fd95ed5f4616cdcd
Andrea Selmi <andreaselmi90@gmail.com>
2022-05-05 12:17:32 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-    res.send('hello worldddddd');
+app.get("/", function (req, res) {
+  res.send("hello worldddddd");
 });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`);
 });
