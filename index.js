9c9d4d4696fae59288787768ceb29694ddff3d54
Andrea Selmi <andreaselmi90@gmail.com>
2022-05-12 21:20:00 +0200

    .

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.use("/email", email);
 
